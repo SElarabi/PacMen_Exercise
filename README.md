e4c5ba1916d78305ead5fb7271effbbb30a6c5ea
Edie Larabi <eddie.larabi@gmail.com>
2023-02-03 21:06:06 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+<!-- @format -->
+
 # PacMen-Exercise
-Exercise to learn how to dynamically inject content into a web page using JavaScript
+
+Exercise to learn how to dynamically inject content into a web page using JavaScript.
+
+This project is a demo website that showcases a JavaScript game. The website includes a navbar with links to other pages, buttons to add PacMan, start, and stop the game, and a description of the game. The game itself is displayed in a container on the page, and uses HTML and CSS for the visual elements. The JavaScript component of the project interacts with the HTML elements to create a dynamic game experience where the position of the elements on the screen updates in real-time .
--- pacmen.js
+++ pacmen.js
@@ -1,84 +1,104 @@
-let pos = 0;
+/** @format */
+
+var pos = 0 + "px";
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ["./images/PacMan1.png", "./images/PacMan2.png"],
+  ["./images/PacMan3.png", "./images/PacMan4.png"],
 ];
-let direction = 0;
-const pacMen = []; // This array holds all the pacmen
 
+// This variable helps determine which PacMan image should be displayed. It flips between values 0 and 1
+var focus = 0;
+// this is array of newpacMen
+const pacMen = [];
 
-// This function returns an object with random values
 function setToRandom(scale) {
   return {
     x: Math.random() * scale,
     y: Math.random() * scale,
   };
 }
-
-// Factory to make a PacMan at a random position with random velocity
+// Factory to make a PacMan
 function makePac() {
-  // returns an object with random values scaled {x: 33, y: 21}
-  let velocity = setToRandom(10); // {x:?, y:?}
-  let position = setToRandom(200);
-
+  // returns an object with values scaled {x: 33, y: 21}
+  let velocity = {
+    x: 33,
+    y: 27,
+  };
   // Add image to div id = game
-  let game = document.getElementById('game');
-  
-  let newimg = document.createElement('img');  
-  newimg.style.position = 'absolute';
-  newimg.src = './images/PacMan1.png ';//pacArray[1].toString;
+  let game = document.getElementById("game");
+  let gameRect = game.getBoundingClientRect();
+  let position = setToRandom(250);
+  let newimg = document.createElement("img");
+  newimg.style.position = "absolute";
+  newimg.src = "./images/PacMan1.png";
   newimg.width = 100;
-  
-  
+  newimg.style.left = position.x + gameRect.left;
+  position.x = position.x + gameRect.left;
+  newimg.style.top = position.y + gameRect.top;
+  position.y = position.y + gameRect.top;
+  let direction = 0;
+  // this variable defines what direction should PacMan go into:
+  // 0 = left to right
+  // 1 = right to left (reverse)
 
-  // TODO: set position here
-  newimg.style.left = position.x ;
-  newimg.style.top  = position.y ;
-
-  // TODO add new Child image to game
   game.appendChild(newimg);
 
-  // return details in an object
+  // new style of creating an object
   return {
     position,
     velocity,
     newimg,
+    direction,
   };
 }
 
 function update() {
-  // loop over pacmen array and move each one and move image in DOM
+  //loop over pacmen array and move each one and move image in DOM
   pacMen.forEach((item) => {
     checkCollisions(item);
+    focus = (focus + 1) % 2;
+    item.newimg.src = pacArray[item.direction][focus];
+
     item.position.x += item.velocity.x;
     item.position.y += item.velocity.y;
 
     item.newimg.style.left = item.position.x;
     item.newimg.style.top = item.position.y;
   });
-  setTimeout(update, 20);
+  setTimeout(update, 150);
 }
 
 function checkCollisions(item) {
-  // TODO: detect collision with all walls and make pacman bounce
+  let game = document.getElementById("game");
+  let gameRect = game.getBoundingClientRect();
+
   if (
-    item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
-    item.position.x + item.velocity.x < 0
-  )
-  
-  item.velocity.x = -item.velocity.x;
-    
+    item.position.x + item.velocity.x + item.newimg.width > gameRect.right ||
+    item.position.x + item.velocity.x < gameRect.left
+  ) {
+    item.velocity.x = -item.velocity.x;
+  }
+
   if (
-    item.position.y + item.velocity.y + item.newimg.height > window.innerHeight ||
-    item.position.y + item.velocity.y < 0
-  )
+    item.position.y + item.velocity.y + item.newimg.height > gameRect.bottom ||
+    item.position.y + item.velocity.y < gameRect.top
+  ) {
     item.velocity.y = -item.velocity.y;
-    
+  }
+  let distanceToedge = gameRect.width - item.position.x;
+
+  if (distanceToedge < item.newimg.width + item.velocity.x) item.direction = 0;
+  if (item.position.x < 0) item.direction = 1;
+
+  //if(pos = 0- imgWidth) direction = 0 ;
+
+  /** if(pos - pageWidth <  imgWidth){
+    direction = 0;*/
+}
+function stop() {
+  clearTimeout(update);
 }
 
 function makeOne() {
   pacMen.push(makePac()); // add a new PacMan
 }
-
-
-
