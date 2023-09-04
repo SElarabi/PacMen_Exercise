369135278b26baf113da40a6c3e3b9a735c59ccf
Eddie Larabi <117444690+SElarabi@users.noreply.github.com>
2023-09-04 12:43:53 -0400

    pacmen update pic direction on edges

--- pacmen.js
+++ pacmen.js
@@ -1,117 +1,127 @@
 /** @format */
 
-var pos = 0 + "px";
+var pos = 0 + 'px';
 const pacArray = [
-  ["./images/PacMan1.png", "./images/PacMan2.png"],
-  ["./images/PacMan3.png", "./images/PacMan4.png"],
+	['./images/PacMan1.png', './images/PacMan2.png'],
+	['./images/PacMan3.png', './images/PacMan4.png'],
 ];
-
 // This variable helps determine which PacMan image should be displayed. It flips between values 0 and 1
-let focus = 1;
+// let focus = 1;
 // this is array of newpacMen
-const pacMen = [];
+const pacMan = [];
 
 function setToRandom(scale) {
-  return {
-    x: Math.random() * scale,
-    y: Math.random() * scale,
-  };
+	return {
+		x: Math.random() * scale,
+		y: Math.random() * scale,
+	};
 }
 // Factory to make a PacMan
 function makePac() {
-  // returns an object with values scaled {x: 33, y: 21}
-  let velocity = {
-    x: 33,
-    y: 27,
-  };
+	// returns an object with values scaled {x: 33, y: 21}
+	let velocity = {
+		x: 33,
+		y: 27,
+	};
 
-  // Add image to div id = game
-  let game = document.getElementById("game");
-  let gameRect = game.getBoundingClientRect();
-  let position = setToRandom(250);
-  let newimg = document.createElement("img");
-  newimg.style.position = "absolute";
-  newimg.src = "./images/PacMan1.png";
-  newimg.width = 100;
-  newimg.style.left = position.x + gameRect.left;
-  position.x = position.x + gameRect.left;
-  newimg.style.top = position.y + gameRect.top;
-  position.y = position.y + gameRect.top;
-  let direction = 0;
-  // this variable defines what direction should PacMan go into:
-  // 0 = left to right
-  // 1 = right to left (reverse)
+	// Add image to div id = game
+	let game = document.getElementById('game');
+	let gameRect = game.getBoundingClientRect();
+	let position = setToRandom(250);
+	let newImg = document.createElement('img');
+	newImg.style.position = 'absolute';
+	newImg.src = './images/PacMan1.png';
+	newImg.width = 100;
+	newImg.style.left = position.x + gameRect.left;
+	position.x = position.x + gameRect.left;
+	newImg.style.top = position.y + gameRect.top;
+	position.y = position.y + gameRect.top;
+	let direction = 0;
 
-  game.appendChild(newimg);
+	// this variable defines what direction should PacMan go into:
+	// 0 = left to right
+	// 1 = right to left (reverse)
+	// This variable helps determine which PacMan image should be displayed. It flips between values 0 and 1
+	let focus = 1;
+	game.appendChild(newImg);
 
-  // new style of creating an object
-  return {
-    position,
-    velocity,
-    newimg,
-    direction,
-  };
+	// new style of creating an object
+	let pacManItem = {
+		position,
+		velocity,
+		newImg,
+		direction,
+		focus,
+	};
+
+	console.log('pacMan ', pacMan);
+	return pacManItem;
 }
 
 function update() {
-  //loop over pacmen array and move each one and move image in DOM
-  pacMen.forEach((item) => {
-    checkCollisions(item);
-    focus = (focus + 1) % 2;
+	//loop over pacMan array and move each one and move image in DOM
+	pacMan.forEach((item) => {
+		checkCollisions(item);
+		item.focus = (item.focus + 1) % 2;
 
-    item.newimg.src = pacArray[item.direction][focus];
+		console.log('direction ', item.direction);
 
-    item.position.x += item.velocity.x;
-    item.position.y += item.velocity.y;
+		item.newImg.src = pacArray[item.direction][item.focus];
+		console.log(
+			'pacArray[ item.direction ][ focus ] ',
+			pacArray[item.direction][item.focus]
+		);
 
-    item.newimg.style.left = item.position.x;
-    item.newimg.style.top = item.position.y;
-  });
+		item.position.x += item.velocity.x;
+		item.position.y += item.velocity.y;
 
-  setTimeout(update, 150);
+		item.newImg.style.left = item.position.x;
+		item.newImg.style.top = item.position.y;
+	});
+
+	setTimeout(update, 150);
 }
 
 function checkCollisions(item) {
-  let game = document.getElementById("game");
-  let gameRect = game.getBoundingClientRect();
+	let game = document.getElementById('game');
+	let gameRect = game.getBoundingClientRect();
+	console.log('gameRect ', gameRect);
+	if (
+		item.position.x + item.velocity.x + item.newImg.width >
+		gameRect.right
+		// ||
+		// item.position.x + item.velocity.x < gameRect.left
+	) {
+		item.velocity.x = -item.velocity.x;
+		item.direction = 1;
+	}
+	if (item.position.x + item.velocity.x < gameRect.left) {
+		item.velocity.x = -item.velocity.x;
+		item.direction = 0;
+	}
+	if (
+		item.position.y + item.velocity.y + item.newImg.height > gameRect.bottom ||
+		item.position.y + item.velocity.y < gameRect.top
+	) {
+		item.velocity.y = -item.velocity.y;
+	}
 
-  if (
-    item.position.x + item.velocity.x + item.newimg.width > gameRect.right ||
-    item.position.x + item.velocity.x < gameRect.left
-  ) {
-    item.velocity.x = -item.velocity.x;
-  }
-
-  if (
-    item.position.y + item.velocity.y + item.newimg.height > gameRect.bottom ||
-    item.position.y + item.velocity.y < gameRect.top
-  ) {
-    item.velocity.y = -item.velocity.y;
-  }
-  let distanceToedge = gameRect.width - item.position.x;
-
-  if (distanceToedge < item.newimg.width + item.velocity.x) item.direction = 0;
-  if (item.position.x < 0) item.direction = 1;
-
-  //if(pos = 0- imgWidth) direction = 0 ;
-
-  /** if(pos - pageWidth <  imgWidth){
-    direction = 0;*/
+	console.log('item.position.x ', item.position.x);
 }
 function stop() {
-  // TO BE CONTINUED
-  pacMen.forEach((item) => {
-    item.velocity.x = 0;
-    item.velocity.y = 0;
+	// TO BE CONTINUED
+	pacMan.forEach((item) => {
+		item.velocity.x = 0;
+		item.velocity.y = 0;
 
-    item.newimg.style.left = item.position.x;
-    item.newimg.style.top = item.position.y;
-  });
+		item.newImg.style.left = item.position.x;
+		item.newImg.style.top = item.position.y;
+	});
 }
 function reset() {
-  location.reload();
+	location.reload();
 }
 
 function makeOne() {
-  pacMen.push(makePac()); // add a new PacMan
+	pacMan.push(makePac()); // add a new PacMan
 }
